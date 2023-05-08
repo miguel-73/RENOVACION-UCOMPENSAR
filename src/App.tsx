3589830b8ad98a68a6f758daa9511530b5e8a535
JhonB19 <bermudezriverajhon@gmail.com>
2023-05-08 15:48:20 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
-import SideBar from './common/SideBar/SideBar';
-import Almacen from './pages/Almacen/Almacen';
-import { Box } from '@mui/material';
+
 
 function App() {
   return (
