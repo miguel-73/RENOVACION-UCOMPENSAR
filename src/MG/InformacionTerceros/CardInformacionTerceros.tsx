74e003f62b6d89ace6ff99268fb310dd49d1b83c
Gxrman-exe <128958285+Gxrman-exe@users.noreply.github.com>
2023-05-08 09:55:26 -0500

    Actualizar: Se actualizo el contenido del archivo CardInformacionTerceros.tsx

--- src/MG/InformacionTerceros/CardInformacionTerceros.tsx
+++ src/MG/InformacionTerceros/CardInformacionTerceros.tsx
@@ -9,7 +9,6 @@ export default function BasicCard() {
         position: "relative",
         minWidth: 275,
         top: "400px",
-        border: 2,
         height: "15%",
         borderRadius: "20px",
         background: " linear-gradient(90deg, rgba(47,217,17,1) 3%, rgba(25,94,13,1) 26%, rgba(17,24,46,1) 56%, rgba(46,138,246,1) 80%);"
