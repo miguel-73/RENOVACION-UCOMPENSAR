82df603f3e56e7199ce2bad5ed844823ba4b5f64
YojansC27M <yojans555cardenas@gmail.com>
2023-05-09 08:56:12 -0500

    Update App.tsx
    Actualizar app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import ParteStiven from './StivenDJ/CONTAINER/Container-Main';
 import { Grid } from '@mui/material';
 
 function App() {
   return (
     <Grid>
-      
+
     </Grid>
   );
 }
