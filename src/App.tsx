5debb23f5e719a773967168244708bcaa6b30390
HectorStiven <132477892+HectorStiven@users.noreply.github.com>
2023-05-11 14:37:54 -0500

    preprarado
    modal completo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import ParteStiven from './StivenDJ/CONTAINER/Container-Main';
 import { Grid } from '@mui/material';
 
 function App() {
   return (
     <Grid>
-
-     <ParteStiven />
 
 
     </Grid>
